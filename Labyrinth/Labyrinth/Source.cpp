514b0f56a3430fbcf890b83b6a3e0e1ed03c933c
KalibLindsey <kalib.lindsey@uky.edu>
2016-04-09 16:47:13 -0400

    getMaze and printMaze Commit

--- Labyrinth/Labyrinth/Source.cpp
+++ Labyrinth/Labyrinth/Source.cpp
@@ -5,23 +5,56 @@
 
 using namespace std;
 
-vector<vector<string>> getMaze(string filename)
-{
+ void getMaze(string filename, vector<vector<int>> &mazeMap)
+{	
 	ifstream myMazeFile(filename);
+	string line;
+	int lineNum;
 	
-	if (myMazeFile.peek() == ifstream::traits_type::eof())
-	
-
+	if (myMazeFile.is_open())
+		while (getline(myMazeFile, line))
+		{
+			vector<int> row;
+			for (int i = 0; i < line.size(); i++)
+				if (line[i] != ' ')
+				{
+					if (line[i] == '0')
+						row.push_back(0);
+					else if (line[i] == '1')
+						row.push_back(1);
+					else if (line[i] == '2')
+						row.push_back(2);
+					//lineNum = atoi(line[i]);
+					//row.push_back(lineNum);
+				}
+			mazeMap.push_back(row);
+		}
+	myMazeFile.close();
 }
 
-void printMaze()
+void printMaze(vector<vector<int>> mazeMap)
 {
+	for (int i = 0; i < mazeMap.size(); i++)
+	{
+		for (int j = 0; j < mazeMap[i].size(); j++)
+		{
+			cout << mazeMap[i][j] << " ";
+		}
+		cout << endl;
+	}
 
 }
 
 int main()
 {
+	string filename = "mazetest.txt";
 
+	vector<vector<int>> mazeMap;
 
+	getMaze(filename, mazeMap);
+	printMaze(mazeMap);
 
+	system("pause");
+
+	return 0;
 }
