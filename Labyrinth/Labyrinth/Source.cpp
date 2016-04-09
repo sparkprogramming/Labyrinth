95d4631d2cf6bbe840c6ecb89ea677dea1ee0f21
KalibLindsey <kalib.lindsey@uky.edu>
2016-04-09 15:36:04 -0400

    Created Main

---
+++ Labyrinth/Labyrinth/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<vector<string>> getMaze(string filename)
+{
+	ifstream myMazeFile(filename);
+	
+	if (myMazeFile.peek() == ifstream::traits_type::eof())
+	
+
+}
+
+void printMaze()
+{
+
+}
+
+int main()
+{
+
+
+
+}
