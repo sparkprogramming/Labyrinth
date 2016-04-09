f01127a4a811553766342e06f0e728b399e16743
dramiot <amiot.dakota.r@gmail.com>
2016-04-09 17:50:30 -0400

    addded movement functions

--- Labyrinth/Labyrinth/Source.cpp
+++ Labyrinth/Labyrinth/Source.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <conio.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -45,16 +47,125 @@ void printMaze(vector<vector<int>> mazeMap)
 
 }
 
+vector<int> findPlayer(vector<vector<int>> mazeMap)
+{
+	vector<int> pos;
+	int row;
+	int col;
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+		{
+			if (mazeMap[i][j] == 2)
+			{
+				row = i;
+				col = j;
+				break;
+			}
+		}
+
+	pos.push_back(row);
+	pos.push_back(col);
+
+	return pos;
+}
+
+void moveUp(vector<vector<int>> &mazeMap, vector<int> pos)
+{
+	if (pos[0] != 0)
+	{
+		if (mazeMap[pos[0] - 1][pos[1]] == 0)
+		{
+			mazeMap[pos[0] - 1][pos[1]] = 2;
+			mazeMap[pos[0]][pos[1]] = 0;
+
+			system("CLS");
+			printMaze(mazeMap);
+		}
+	}
+}
+
+void moveLeft(vector<vector<int>> &mazeMap, vector<int> pos)
+{
+	if (pos[1] != 0)
+	{
+		if (mazeMap[pos[0]][pos[1] - 1] == 0)
+		{
+			mazeMap[pos[0]][pos[1] - 1] = 2;
+			mazeMap[pos[0]][pos[1]] = 0;
+
+			system("CLS");
+			printMaze(mazeMap);
+		}
+	}
+}
+
+void moveDown(vector<vector<int>> &mazeMap, vector<int> pos)
+{
+	if (pos[0] != 9)
+	{
+		if (mazeMap[pos[0] + 1][pos[1]] == 0)
+		{
+			mazeMap[pos[0] + 1][pos[1]] = 2;
+			mazeMap[pos[0]][pos[1]] = 0;
+
+			system("CLS");
+			printMaze(mazeMap);
+		}
+	}
+}
+
+void moveRight(vector<vector<int>> &mazeMap, vector<int> pos)
+{
+	if (pos[1] != 9)
+	{
+		if (mazeMap[pos[0]][pos[1] + 1] == 0)
+		{
+			mazeMap[pos[0]][pos[1] + 1] = 2;
+			mazeMap[pos[0]][pos[1]] = 0;
+
+			system("CLS");
+			printMaze(mazeMap);
+		}
+	}
+}
+
+
 int main()
 {
 	string filename = "mazetest.txt";
 
 	vector<vector<int>> mazeMap;
 
+	vector<int> playerPos;
+
 	getMaze(filename, mazeMap);
 	printMaze(mazeMap);
 
-	system("pause");
+	playerPos = findPlayer(mazeMap);
+
+	char direction = _getch();
+	while (direction != 'q')
+	{
+		if (direction == 'w')
+		{
+			moveUp(mazeMap, findPlayer(mazeMap));
+		}
+		else if (direction == 'a')
+		{
+			moveLeft(mazeMap, findPlayer(mazeMap));
+		}
+		else if (direction == 's')
+		{
+			moveDown(mazeMap, findPlayer(mazeMap));
+		}
+		else if (direction == 'd')
+		{
+			moveRight(mazeMap, findPlayer(mazeMap));
+		}
+
+		direction = _getch();
+	}
+
 
 	return 0;
 }
